fc01e4b9aec6c4f45aab2d9d1449b91d1456861e
mezriss <mezriss@gmail.com>
2023-03-05 22:57:04 +0200

    Initial

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+    import { base } from "$app/paths";
+</script>
+
+<svelte:head>
+    <title>Disgaea RPG Info</title>
+    <meta name="description" content="Disgaea RPG character stats and more" />
+</svelte:head>
+
+<h1>Disgaea RPG Info</h1>
+<a href="{base}/characters">Characters</a>
